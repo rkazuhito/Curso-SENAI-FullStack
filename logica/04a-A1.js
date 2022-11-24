48e7e54c1d31b2f7cb2f996fbd7f1190494a4caf
Rafael Kazuhito Vidal Beltrão <115881851+rkazuhito@users.noreply.github.com>
2022-11-24 00:54:18 -0300

    a1

---
+++ logica/04a-A1.js
@@ -0,0 +1,23 @@
+let peso = 50
+if(peso<100){
+    console.log("A peça deve ter no minimo 100g")
+}else{
+console.log("A peça possui o tamanho adequado")
+}
+
+var listaDePecas = ["Farol","Motor","Cambio","Painel","Cabeçote","Radiador"]
+if(listaDePecas.length <10){
+    //é possível cadastrar
+    console.log("É possível cadastrar mais")
+} else{
+    console.log("Sem espaço")
+}
+
+
+
+let nomepeca = "motor"
+if(nomepeca.length>3){
+console.log("nome da peça adequado")
+}else{
+    console.log("o nome da peca deve ter mais que 3 carcteres, digite o numero adquado")
+}
